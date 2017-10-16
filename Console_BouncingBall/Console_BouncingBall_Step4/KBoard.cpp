909377e5b4860b56124fdea742a530c41214b8da
jintaeks <jintaeks@gmail.com>
2017-10-17 03:00:52 +0900

    Added Console Bouncing Ball Step 4. jintaeks on 2017-10-17_03-00

---
+++ Console_BouncingBall/Console_BouncingBall_Step4/KBoard.cpp
@@ -0,0 +1,57 @@
+#include "KBoard.h"
+
+
+void KBoard::Initialize()
+{
+    ::memset( (char*)m_aBoard, 0, BOARD_SIZE_ROW * BOARD_SIZE_COL * sizeof( int ) );
+}
+
+void KBoard::CopyFrom( const KBoard& rhs_ )
+{
+    memcpy( m_aBoard, rhs_.m_aBoard, sizeof( m_aBoard ) );
+}
+
+bool KBoard::GetBoardCellValue( int* pCellValue_, const KPoint worldPos_ ) const
+{
+    if( IsValidBoardPos( worldPos_ ) == true ) {
+        // set [out] parameter
+        *pCellValue_ = m_aBoard[worldPos_.y][worldPos_.x];
+        return true;
+    }//if
+    return false;
+}
+
+void KBoard::LocateBlock( const KBlock& block_ )
+{
+    for( int iStone = 0; iStone < block_.m_iNumStone; ++iStone ) {
+        // transform local coordinate to global coordinate
+        const KPoint worldPos = block_.GetPos( iStone );
+        // draw a block on the board
+        const bool isValid = IsValidBoardPos( worldPos );
+        if( isValid == true ) {
+            m_aBoard[worldPos.y][worldPos.x] = '#';
+        }//if
+    }//for
+}
+
+bool KBoard::CanLocateBlock( const KBlock& block_ ) const
+{
+    for( int iStone = 0; iStone < block_.m_iNumStone; ++iStone ) {
+        const KPoint worldPos = block_.GetPos( iStone );
+        int cellValue = 0;
+        const bool bIsGetValue = GetBoardCellValue( OUT &cellValue, worldPos );
+        if( bIsGetValue == false || cellValue != 0 )
+            return false;
+    }//for
+    return true;
+}
+
+int KBoard::IsCollide(const KBlock& block_) const
+{
+    if (block_.m_worldPos.x <= 1 || block_.m_worldPos.x >= BOARD_SIZE_COL-1)
+        return 1;
+    else if (block_.m_worldPos.y <= 1 || block_.m_worldPos.y >= BOARD_SIZE_ROW-1)
+        return 2;
+
+    return 0;
+}
